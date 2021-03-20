d70fc9d83183c8e6f7e8d5f867afe0a101884e47
Agus Nindo <agusnindo@gmail.com>
2021-03-20 22:37:58 +0700

    navigasi sudah berhasil Responsive

--- index.php
+++ index.php
@@ -5,40 +5,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Impelementasi Design CSS sendiri Tanpa Framework</title>
+    <title>Document</title>
 
-
+    <!-- Css Sendiri -->
     <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-
     <nav>
-        <a href="#" class="box-brand"><span>Brand</span></a>
-        <ul class="box-right">
+
+        <button class="toggle-btn" id="tombol">=</button>
+
+        <a href="#" class="box-brand"> <span>Brand</span></a>
+        <ul class="box-navigasi" id="menu">
             <li>
                 <a href="#">Home</a>
             </li>
             <li>
-                <a href="#">About</a>
-            </li>
-            <li>
                 <a href="#">Contact</a>
             </li>
+            <li>
+                <a href="#">About</a>
+            </li>
         </ul>
-
-        <div class="form-pencarian">
-            <form action="">
-                <input type="text" placeholder="Cari Informasi">
-                <button>Cari</button>
-            </form>
-        </div>
-
-
     </nav>
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias vero minima voluptatum aliquid labore rerum commodi neque esse ipsum perspiciatis magni nulla quidem, nostrum, veniam voluptas sequi fugit saepe deserunt?</p>
-
 </body>
+
+
+<!-- Javascript Library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<script>
+    var ukuranDekstop = window.matchMedia("screen and (min-width: 501px)");
+    // var ukuranMobile = window.matchMedia("screen and (max-width: 500px)");
+    $(document).ready(function() {
+        $('#tombol').on('click', function() {
+            $('#menu').toggle("slow");
+        });
+        $(window).resize(function() {
+            if (ukuranDekstop.matches) {
+                $("#menu").show();
+                $("#tombol").hide();
+            }else{
+                $("#tombol").show();
+                $("#menu").hide();
+            }
+        });
+    });
+</script>
 
 </html>
