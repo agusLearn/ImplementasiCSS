2b990b5ed11bcab7d939bd085dbbdb2e3ad3c780
Agus Nindo <agusnindo@gmail.com>
2021-03-20 18:28:45 +0700

    Penambahan CSS dan pembuatan Navigasi, masih diperbaiki

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Impelementasi Design CSS sendiri Tanpa Framework</title>
+
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <nav>
+        <a href="#" class="box-brand"><span>Brand</span></a>
+        <ul class="box-right">
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+
+        <div class="form-pencarian">
+            <form action="">
+                <input type="text" placeholder="Cari Informasi">
+                <button>Cari</button>
+            </form>
+        </div>
+
+
+    </nav>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias vero minima voluptatum aliquid labore rerum commodi neque esse ipsum perspiciatis magni nulla quidem, nostrum, veniam voluptas sequi fugit saepe deserunt?</p>
+
+</body>
+
+</html>
