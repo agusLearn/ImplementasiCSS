c549330d02ca8a16c10abac2a2bf362ffb7335e9
Agus Nindo <agusnindo@gmail.com>
2021-03-21 14:24:00 +0700

    Penambahan File login.php(belum ditulis kodenya) dan penambahan CSS grid system

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Index</title>
 
     <!-- Css Sendiri -->
     <link rel="stylesheet" href="css/style.css">
@@ -42,6 +42,8 @@
         $('#tombol').on('click', function() {
             $('#menu').toggle("slow");
         });
+
+        
         $(window).resize(function() {
             if (ukuranDekstop.matches) {
                 $("#menu").show();
