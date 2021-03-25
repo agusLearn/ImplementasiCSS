00a349d929a81cc5828279d15570c80ff0993b7f
Agus Nindo <agusnindo@gmail.com>
2021-03-25 19:43:48 +0700

    Mendisain Halaman Home dan membuat CSS dengan tampilan yang menarik. masih dalam tahap pengembangan dan perbaikan

---
+++ halamanUtama.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Design Halaman Utama</title>
+
+    <!-- Library CSS Font Awesome Online -->
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+    <!-- Library Google Font Online -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300&display=swap" rel="stylesheet">
+
+    <!-- CSS buatan Sendiri -->
+    <link rel="stylesheet" href="css/style2.css">
+
+</head>
+
+<body>
+
+    <!-- Navigasi Bar -->
+    <nav class="navigasi">
+        <a href="#" class="branded"><span>Max</span>Nyuuss</a>
+
+        <div class="menu-tengah">
+            <ul>
+                <li>
+                    <a href="#">Pencake</a>
+                </li>
+                <li>
+                    <a href="#">Burger</a>
+                </li>
+                <li>
+                    <a href="#">Wuffle</a>
+                </li>
+                <li>
+                    <a href="#">Beferage</a>
+                </li>
+            </ul>
+
+            <form action="">
+                <input type="text" placeholder="Search ..">
+                <button type="submit"><span><i class="fas fa-search"></i></span></button>
+            </form>
+
+        </div>
+
+        <div class="menu-right">
+            <ul>
+                <li>
+                    <a href="#"><i class="fas fa-heart"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fas fa-shopping-basket"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fas fa-user-circle"></i></a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- Penutup Navigasi Bar -->
+
+    <!-- Content -->
+
+    <div class="container">
+
+        <div class="row menu-content">
+
+            <!-- Content Sebelah Kiri -->
+            <div class="col-d-9">
+                <div class="row">
+                    <div class="col-d-12">
+                        <ul>
+                            <li>
+                                <a href="#">Allvariant</a>
+                            </li>
+                            <li>
+                                <a href="#">Banana</a>
+                            </li>
+                            <li>
+                                <a href="#">Strawberry</a>
+                            </li>
+                            <li>
+                                <a href="#">BlueBerry</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <!-- Gambar Makanan -->
+                    <div class="col-d-12">
+                        
+                    </div>
+
+                </div>
+            </div>
+
+            <!-- Content Sebelah Kanan -->
+            <div class="col-d-3">
+
+                <div class="row">
+
+                    <div class="col-d-12">
+                        <h1>Original Waffle</h1>
+                    </div>
+
+                    <div class="col-d-12">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Eveniet minus distinctio est voluptate voluptatum repellat.
+                            Minus magni, delectus veniam labore iure id pariatur illum nemo
+                            reiciendis nisi neque? Animi, debitis!</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+
+</body>
+
+</html>
